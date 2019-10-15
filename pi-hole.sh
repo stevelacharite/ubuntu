fda1f40c7d216937ac8738a021631e2c61f95642
stevelacharite <stevelacharite@gmail.com>
2019-10-15 08:09:49 -0400

    Update pi-hole.sh

--- pi-hole.sh
+++ pi-hole.sh
@@ -2,3 +2,15 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 sudo wget -O pihole-install.sh https://install.pi-hole.net
 
+sudo chmod +x pihole-install.sh
+
+sudo ./pihole-install.sh
+
+#http://172.16.48.5/admin
+
+#password est dans le résultat du script
+
+#Listes: 
+#https://wally3k.github.io/
+
+#(prendre ceux avec crochet seulement)
